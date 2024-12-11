934914d4e7252cf8a5d98486038bc6ca63e7fcdf
kibersashka <aleksandraveldaeva4@gmail.com>
2024-12-11 11:59:47 +0300

    Add class Boat

--- src/src/main/java/org/example/modul/Boat.java
+++ src/src/main/java/org/example/modul/Boat.java
@@ -1,4 +1,40 @@
 package org.example.modul;
 
-public class Boat {
+public class Boat extends Objects{
+    private String typeBodyWater;
+    private int distance;
+
+
+
+    public Boat(int speed, int age, String name, int distance, String typeBodyWater) {
+        super(name, speed);
+        this.typeBodyWater = typeBodyWater;
+        this.distance = distance;
+    }
+
+    public String getTypeBodyWater() {
+        return typeBodyWater;
+    }
+
+    public void setTypeBodyWater(String typeBodyWater) {
+        this.typeBodyWater = typeBodyWater;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Время в пути: " + ((double) (distance) / (double) (speed)));
+    }
+
+    public String sail() {
+        return name + "начал движение по " + typeBodyWater;
+    }
+
 }
