ce8b77bcefb580963727cb5237c6617bbc021955
kibersashka <aleksandraveldaeva4@gmail.com>
2024-12-11 12:01:07 +0300

    Add class SportsCar (car)

--- src/src/main/java/org/example/modul/SportsCar.java
+++ src/src/main/java/org/example/modul/SportsCar.java
@@ -1,4 +1,31 @@
 package org.example.modul;
 
-public class SportsCar {
+
+public class SportsCar extends Car {
+    private int power;
+
+    public SportsCar() {
+    }
+
+    public SportsCar(int speed, int age, int power, String name) {
+        super(speed, age, name);
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Мощности хватит на " + ((double) (power) / (double) (speed)));
+    }
+    public String driveSportsCar() {
+        return "Количество топлива: " + power;
+    }
+
 }
