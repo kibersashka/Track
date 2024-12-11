4721a61386ce9e92c074f33c0507474f051645e3
kibersashka <aleksandraveldaeva4@gmail.com>
2024-12-11 12:00:39 +0300

    Add class ElectroCar (car)

--- src/src/main/java/org/example/modul/ElectroCar.java
+++ src/src/main/java/org/example/modul/ElectroCar.java
@@ -1,4 +1,30 @@
 package org.example.modul;
 
-public class ElectroCar {
+public class ElectroCar extends Car{
+    protected int battery;
+
+    public ElectroCar() {
+
+    }
+
+    public ElectroCar(int speed, int age, int battery, String name){
+        super(speed, age, name);
+        this.battery = battery;
+    }
+
+    public int getBattery() {
+        return battery;
+    }
+
+    public void setBattery(int battery) {
+        this.battery = battery;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Заряда хватит на " + ((double) (battery) / (double)(speed)) + " минут");
+    }
+    public String driveElectroCar() {
+        return "Заряд батареи: " + battery;
+    }
 }
