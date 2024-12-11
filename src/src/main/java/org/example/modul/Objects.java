652e08c70176d2b43113f1dc021dfb54fbd93bb5
kibersashka <aleksandraveldaeva4@gmail.com>
2024-12-11 11:55:56 +0300

    Add class Object - parent class

--- src/src/main/java/org/example/modul/Objects.java
+++ src/src/main/java/org/example/modul/Objects.java
@@ -1,4 +1,38 @@
 package org.example.modul;
 
 public class Objects {
+    protected String name;
+    protected int speed;
+
+    public Objects() {
+    }
+
+    public Objects(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void start() {
+        System.out.println("Двигатель запущен");
+    }
+
+
 }
+
+
